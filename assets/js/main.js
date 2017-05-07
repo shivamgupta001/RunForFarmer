68ff84dd691e8fbe09eb3c1ff17f3a8c3795d2a6
shivamgupta001 <shivamcs3080@gmail.com>
2017-05-07 15:09:20 +0530

    adde register and events page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,18 @@
-$(".nav a").on("click", function(){
-   $(".nav").find(".active").removeClass("active");
-   $(this).parent().addClass("active");
+
+	
+
+$(document).ready(function () {
+    
+	//navigation bar active class switching
+	$(".nav a").on("click", function(){
+		$(".nav").find(".active").removeClass("active");
+		$(this).parent().addClass("active");
+	});
+
+	//navigation bar on mobile view on item click close dropdown
+    $(".navbar-nav li a").click(function(event) {
+        $('button.navbar-toggle').click()
+    });
+
 });
+
